f59738aab0ea9aa0d6abfaa5e56253e1cb878e7a
Jesus Bustillo <jesus.bustillo.barrios@gmail.com>
2022-08-26 21:13:27 +0100

    test

--- src/main/java/com/foodplanner/searchrecipe/SearchRecipeApplication.java
+++ src/main/java/com/foodplanner/searchrecipe/SearchRecipeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SearchRecipeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearchRecipeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SearchRecipeApplication.class, args);
+    }
 
 }
